523c4d11b01e006287d71d55e92be79f20eb7d09
Greg Baker <greg.baker.bsc@gmail.com>
2015-11-07 18:23:46 -0330

    Added empty servlet class

---
+++ src/main/java/com/github/xxbeanxx/servlet/ClasspathStaticContentServlet.java
@@ -0,0 +1,12 @@
+package com.github.xxbeanxx.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author Greg Baker
+ */
+public class ClasspathStaticContentServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 6266640446993571859L;
+
+}
